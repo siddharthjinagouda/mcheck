511d2df06d2ae1c251f909dcd74427bda9baeb6b
siddharthjinagouda <siddharthjinagouda@gmail.com>
2014-11-29 23:29:23 +0530

    category view

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :products
   has_one :category
   belongs_to :category
+
+  def parent_category
+    Category.find(category_id)
+  end
 end
